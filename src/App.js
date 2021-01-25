63a76db11de56b7b31c0058e9bdc9c2c6a1952d2
Igor Vieira <igor.p.r.vieira@gmail.com>
2021-01-25 17:11:41 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ listItem, setListItem ] = useState([]);
+  const [ item, setItem ] = useState({ id: '', text: '' });
+
+  const handleOnChange = (e) =>
+    setItem({
+      id: `${e.target.value}-${Math.random() * 100}`,
+      text: e.target.value
+    })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (item?.text.length === 0) {
+      return
+    }
+
+    setListItem([...listItem, item])
+    setItem({ id: '', text: '' })
+
+  }
+
+  const handleDelete = (id) => {
+    const newList = listItem?.filter(x => id !== x?.id)
+    setListItem(newList)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hey Everyone!</div>
+      <ul data-testid="todo-list">
+        {
+          listItem?.map((item, index) => (
+            <li key={index}>
+              <div style={{ display: "flex", marginBottom: 20 }}>
+                <div>{item?.text}</div>
+                <button
+                  style={{ marginLeft: "12px", cursor: "pointer" }}
+                  data-testid={`remove-button-${index}`}
+                  onClick={() => handleDelete(item?.id)}
+                >
+                  x
+                </button>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="item"
+          value={item?.text}
+          data-testid="input-text"
+          onChange={(e) => handleOnChange(e)}
+        />
+        <button data-testid="submit">Submit</button>
+      </form>
     </div>
   );
 }
